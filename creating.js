5a85c06ee111a82b8a3fd3be141a5ab351613d96
rahul-chib <43586287+rahul-chib@users.noreply.github.com>
2019-03-14 15:43:37 -0400

    Coded creating.js

--- creating.js
+++ creating.js
@@ -1,15 +1,40 @@
-console.log(data);
 var countries = [];
+var currentCountry = "Afghanistan";
 
 for(let i = 0; i < data.length; i++) {
-    if(i % 5 == 0) {
-        countries.push(data[i].dims.COUNTRY)
+    if(countries.includes(data[i].dims.COUNTRY)) {
+        continue;
+    } else {
+        countries.push(data[i].dims.COUNTRY);
     }
 }
 
-console.log(countries)
+console.log(countries);
+
 var select = document.getElementById("myselect");
+var canvas = document.getElementById("mycanvas");
+var ctx = canvas.getContext("2d");
 
 for(i = 0; i < countries.length; i++) {
     select.options[select.options.length] = new Option(countries[i], countries[i]);
+}
+
+function change_myselect() {
+    currentCountry = select.value;
+    console.log(currentCountry)
+    for (i=0;data.length;i=i+1) {
+            if (data[i].dims.COUNTRY == currentCountry) {
+                var dumbnum = data[i].Value;
+                if(dumbnum == "No data") {
+                    ctx.fillText("This country does not have rabies", 20, 20, 200);
+                    break;
+                }
+                var smartstr = dumbnum.replace("","");
+                var myint = parseInt(smartstr);
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                ctx.fillStyle = "red";
+                ctx.fillRect(0,0,myint + 100,50);
+                break;
+                }
+        }
 }
